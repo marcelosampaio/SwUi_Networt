09398bb06782c164f8191bd76ee27ee86036c262
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2022-05-19 08:43:35 -0300

    Initial Commit

---
+++ SwUi_Networt/SwUi_NetwortApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwUi_NetwortApp.swift
+//  SwUi_Networt
+//
+//  Created by Marcelo Sampaio on 19/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwUi_NetwortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
